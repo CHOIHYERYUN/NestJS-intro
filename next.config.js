8b787382505a91ad28db92849fe5e3fc13532719
HyeRyun Choi <violetmouce@gmail.com>
2022-04-08 22:32:09 +0900

    2-2 redirect and rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const API_KEY = "7ba5d65637647b14a1c44c3d2baf48cc";
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async redirects(){
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ]
+  }
+};
